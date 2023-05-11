70e2eab61012f0e21d8bb31ec2872d84b4f46fdf
Rudra <Rudrayyaswamims@gmail.com>
2023-05-11 07:20:39 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "ec2" {
 # DNS record creation
 resource "aws_route53_record" "record" {
   zone_id = "Z0900206KGOHOIP4AMC1"
-  name    = "$(var.component)-dev.devopsm71.online"
+  name    = "${var.component}-dev.devopsm71.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
--- vars.tf
+++ vars.tf
@@ -10,7 +10,7 @@ variable "instances" {
     }
     catalogue = {
       name  = "catalogue"
-      type = "t3.small"
+      type = "t3.micro"
     }
     redis = {
       name  = "redis"
