856e518059dff3e9254ebc586434d2f638bbd769
Rudra <Rudrayyaswamims@gmail.com>
2023-05-13 23:16:54 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 # data source
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = devops-practice-with-ansible   # ami name
+  name_regex       = "devops-practice-with-ansible"   # ami name
   owners           = [data.aws_caller_identity.current.account_id]
 
 }
