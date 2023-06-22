af8cbfbfce4ecf64649198f397c5dfe46c5b9a20
Rudra <Rudrayyaswamims@gmail.com>
2023-06-22 13:55:18 +0530

    updating roboshop-infra

--- ec2/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-# data source
-data "aws_ami" "ami" {
-  most_recent      = true
-    ## using Raghu's image
-  name_regex  = "Centos-8-DevOps-Practice"   #devops-practice-with-ansible
-  owners      = ["973714476881"]              #[dara.aws_caller_identity.current.account_id]
-}
