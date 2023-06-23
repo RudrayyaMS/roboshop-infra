51ca5e4d3e23f75c519ce8d28552fbafed13f6ec
Rudra <Rudrayyaswamims@gmail.com>
2023-06-23 17:30:35 +0530

    updating roboshop-infra

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ module "vpc" {
   source = "git::https://github.com/RudrayyaMS/tf-module-vpc.git"
   env = var.env
   tags = var.tags
-
+  default_vpc_id = var.default_vpc_id
+  default_route_table = var.default_route_table
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
   public_subnets = each.value["public_subnets"]
